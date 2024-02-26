791bb83f3219fd28dc73bcf6c2b50b4da080c822
Henry <henrybecker1014@gmail.com>
2024-02-26 14:51:19 -0500

    Bulma is working

--- client/src/pages/HomeView.vue
+++ client/src/pages/HomeView.vue
@@ -3,7 +3,12 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class= "hero is-prmary is-large">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Welcome to the 2024 Spring Web Server Programming</h1>
+        <h1 class="subtitle">This is the home page</h1>
+      </div>
+    </div>
   </main>
 </template>
